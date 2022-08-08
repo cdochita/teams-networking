8b4295ef2c221217920a786f9613041564663caa
Catalin Dochita <catalindochita@yahoo.com>
2022-08-08 20:41:04 +0300

    prepare submit team

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var a = "";
 function getTeamHTML(team) {
   return `<tr>
           <td>${team.promotion}</td>
@@ -14,7 +15,7 @@ function dispalyTeams(teams) {
   //transforma in HTML
 
   var teamsHTML = teams.map(getTeamHTML);
-  console.info("teamsHTML", teamsHTML);
+  // console.info("teamsHTML", teamsHTML);
 
   //Afiseaza
   document.querySelector("table tbody").innerHTML = teamsHTML.join("");
@@ -23,12 +24,35 @@ function dispalyTeams(teams) {
 function loadTeams() {
   fetch("data/teams.json")
     .then(function (r) {
-      console.info(r);
+      // console.info(r);
       return r.json();
+      r.json.apply();
     })
     .then(function (teams) {
       dispalyTeams(teams);
     });
 }
+function submitForm(e) {
+  var promotion = document.querySelector("[name=promotion]").value;
+  var members = document.querySelector("[name=members]").value;
+  var name = document.querySelector("[name=name]").value;
+  var url = document.querySelector("[name=url]").value;
+  var team = {
+    promotion: promotion,
+    members: members,
+    name: name,
+    url: url,
+  };
+
+  console.warn("submit", JSON.stringify(team));
+
+  e.preventDefault();
+}
+function initEvents() {
+  var form = document.getElementById("editForm");
+  console.info("form", form);
+  form.addEventListener("submit", submitForm);
+}
 
 loadTeams();
+initEvents();
